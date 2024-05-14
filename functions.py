0cff95b3f337d3ffd2da96e9b87dad997421fd0a
Izaro Fernandez Iriondo <50079042+ifiriondo@users.noreply.github.com>
2024-05-14 22:04:06 +0200

    code formatting

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import os
+import pandas as pd
+import numpy as np
+import xarray as xr
 
 def process_data(data_dir):
     # List of files in the directory
@@ -24,4 +28,34 @@ def process_data(data_dir):
                                     'timepoint': np.arange(np.shape(data_3d)[1]), 
                                     'struc': headers})
 
-    return data_3d
+    return data_3d, data_array, headers
+
+
+def calc_corr_matrix(data_array, headers):
+    # Initialize an empty list to hold the correlation matrices
+    correlation_matrices = []
+
+    # Loop over all subjects to compute correlation matrices
+    for sub in data_array.subject.values:
+        specific_subject_data = data_array.sel(subject=sub)
+        specific_subject_df = specific_subject_data.to_pandas()
+        correlation_matrix = specific_subject_df.corr()
+        correlation_matrices.append(correlation_matrix.values)
+
+    # Convert the list of correlation matrices to a 3D numpy array
+    correlation_3d = np.array(correlation_matrices)
+
+    # Create coordinates for the new DataArray
+    corr_coords = {
+        'subject': [f'Subject{i:03d}' for i in range(1, len(correlation_3d) + 1)],
+        'struc1': headers,
+        'struc2': headers
+    }
+
+    # Create the xarray DataArray for the correlation matrices
+    correlation_data_array = xr.DataArray(correlation_3d, dims=['subject', 'struc1', 'struc2'], coords=corr_coords)
+
+    # Convert to xarray Dataset if needed
+    correlation_dataset = xr.Dataset({'correlation_matrix': correlation_data_array})
+
+    return correlation_3d, correlation_dataset, correlation_data_array
